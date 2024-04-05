fbdd3ad6e0789d53cd00eba2f7c03e6aadc7394a
yuko-izumi <yuko.izumi@woven-planet.global>
2024-04-05 13:59:51 +0900

    module4-01-challenge_04

--- 01_various/04_challenge/main.cpp
+++ 01_various/04_challenge/main.cpp
@@ -1,1 +1,19 @@
-// コードを入力してください
+#include "unique_ptr.hpp"
+
+#include <iostream>
+
+int main() {
+    // unique_int_ptr インスタンスを作成
+    unique_int_ptr my_int(3);
+
+    // 間接参照して値を表示
+    std::cout << *my_int << std::endl;
+
+    // ポインタの制御を放棄
+    int* released_ptr = my_int.release();
+
+    // メモリを解放
+    delete released_ptr;
+
+    return 0;
+}
--- 01_various/04_challenge/unique_ptr.hpp
+++ 01_various/04_challenge/unique_ptr.hpp
@@ -1,1 +1,66 @@
-// コードを入力してください
+class unique_int_ptr {
+private:
+    int* ptr;
+
+public:
+    // コンストラクタ: ヒープに整数を割り当てる
+    unique_int_ptr(int value) {
+        ptr = new int(value);
+    }
+
+    // デストラクタ: 動的に割り当てられたメモリをクリーンアップ
+    ~unique_int_ptr() {
+        delete ptr;
+    }
+
+    // 格納されたポインタを返す
+    int* get() {
+        return ptr;
+    }
+
+    // ポインタの制御を放棄する
+    int* release() {
+        int* released_ptr = ptr;
+        ptr = nullptr;
+        return released_ptr;
+    }
+
+    // 独自の operator* を定義して間接参照をサポート
+    int& operator*() {
+        return *ptr;
+    }
+};
+
+template<typename T>
+class unique_ptr {
+private:
+    T* ptr;
+
+public:
+    // コンストラクタ: ヒープに整数を割り当てる
+    unique_ptr(T value) {
+        ptr = new T(value);
+    }
+
+    // デストラクタ: 動的に割り当てられたメモリをクリーンアップ
+    ~unique_ptr() {
+        delete ptr;
+    }
+
+    // 格納されたポインタを返す
+    T* get() {
+        return ptr;
+    }
+
+    // ポインタの制御を放棄する
+    T* release() {
+        T* released_ptr = ptr;
+        ptr = nullptr;
+        return released_ptr;
+    }
+
+    // 独自の operator* を定義して間接参照をサポート
+    T& operator*() {
+        return *ptr;
+    }
+};
