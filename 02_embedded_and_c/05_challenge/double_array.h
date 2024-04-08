6d5fec0ae1eb93d96c7563b6244792fa87a8afea
yuko-izumi <yuko.izumi@woven-planet.global>
2024-04-08 14:29:24 +0900

    module4-02-challenge_05

---
+++ 02_embedded_and_c/05_challenge/double_array.h
@@ -0,0 +1,17 @@
+// double_array.h
+#ifndef DOUBLE_ARRAY_H
+#define DOUBLE_ARRAY_H
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void double_array(int *arr, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // DOUBLE_ARRAY_H
