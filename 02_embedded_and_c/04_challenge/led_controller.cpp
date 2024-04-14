13b18db2de1a3c88b0a8f4f0b60e77ea2ac565da
yuko-izumi <yuko.izumi@woven-planet.global>
2024-04-15 08:33:37 +0900

    module4-02-04_challenge

--- 02_embedded_and_c/04_challenge/led_controller.cpp
+++ 02_embedded_and_c/04_challenge/led_controller.cpp
@@ -1,1 +1,21 @@
-// コードを入力してください
+#include <iostream>
+
+#include "led_controller.hpp"
+
+LedController::LedController(){
+    led_init();
+}
+
+void LedController::turnOn(){
+    led_on();
+//    ledState = true;
+}
+
+void LedController::turnOff(){
+    led_off();
+//    ledState = false;
+}
+
+bool LedController::getState(){
+    return led_get_state();
+}
--- 02_embedded_and_c/04_challenge/main.cpp
+++ 02_embedded_and_c/04_challenge/main.cpp
@@ -1,1 +1,19 @@
-// コードを入力してください
+#include "led_controller.hpp"
+
+#include <iostream>
+
+int main(){
+    LedController ledcontroller;
+
+    ledcontroller.turnOn();
+    if(ledcontroller.getState()){
+        std::cout << "LED is ON." << std::endl;
+    }
+
+    ledcontroller.turnOff();
+    if(!ledcontroller.getState()){
+        std::cout << "LED is OFF." << std::endl;
+    }
+
+    return 0;
+}
